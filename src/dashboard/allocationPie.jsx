cc540a9f5813d3e318a811cc18f9d41c35ebf44a
prajwal kotalwar <prajwalkotalwar@gmail.com>
2020-07-05 19:37:59 +0530

    added hbar and linked with pie chart

--- src/dashboard/allocationPie.jsx
+++ src/dashboard/allocationPie.jsx
@@ -1,13 +1,105 @@
 import React, { Component } from 'react';
 import { PieChart, Pie, Sector, Cell } from 'recharts';
-
-
-const data = [
-	{ name: 'Group A', value: 400 },
-	{ name: 'Group B', value: 300 },
-	{ name: 'Group C', value: 300 },
-	{ name: 'Group D', value: 200 },
-];
+import HSBar from "react-horizontal-stacked-bar-chart";
+//import axios from 'axios';
+let dummyData = {
+	"assetAllocation": {
+		"responseData": {
+			"allocationDetails": [
+				{
+					"name": "Computer Department",
+					"assetAllotted": 400
+				},
+				{
+					"name": "Electronics Department",
+					"assetAllotted": 200
+				},
+				{
+					"name": "Production Department",
+					"assetAllotted": 300
+				}
+			],
+			"totalAssets": 200
+		},
+		"responseStatus": "OK",
+		"responseMessage": ""
+	},
+	"buAssetAllocation1": {
+		"responseData": {
+			"allocationDetails": [
+				{
+					"name": "Sub BU 1",
+					"assetAllotted": 13
+				},
+				{
+					"name": "Sub BU 2",
+					"assetAllotted": 5
+				},
+				{
+					"name": "Sub BU 3",
+					"assetAllotted": 7
+				},
+				{
+					"name": "Sub BU 4",
+					"assetAllotted": 8
+				},
+				{
+					"name": "Sub BU 5",
+					"assetAllotted": 9
+				},
+				{
+					"name": "Sub BU 6",
+					"assetAllotted": 7
+				}
+			],
+			"totalAssets": 50
+		},
+		"responseStatus": "OK",
+		"responseMessage": ""
+	},
+	"buAssetAllocation2": {
+		"responseData": {
+			"allocationDetails": [
+				{
+					"name": "Sub BU 1",
+					"assetAllotted": 13
+				},
+				{
+					"name": "Sub BU 2",
+					"assetAllotted": 5
+				},
+				{
+					"name": "Sub BU 3",
+					"assetAllotted": 17
+				}
+			],
+			"totalAssets": 50
+		},
+		"responseStatus": "OK",
+		"responseMessage": ""
+	},
+	"buAssetAllocation3": {
+		"responseData": {
+			"allocationDetails": [
+				{
+					"name": "Sub BU 1",
+					"assetAllotted": 13
+				},
+				{
+					"name": "Sub BU 2",
+					"assetAllotted": 15
+				},
+				{
+					"name": "Sub BU 3",
+					"assetAllotted": 7
+				}
+			],
+			"totalAssets": 50
+		},
+		"responseStatus": "OK",
+		"responseMessage": ""
+	}
+}
 
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
@@ -26,25 +118,101 @@ const renderCustomizedLabel = ({
 	);
 };
 
-export default class Example extends Component {
+export default class allocationPie extends Component {
+	constructor(props) {
+		super(props);
+		this.onChangeBar = this.onChangeBar.bind(this);
+		this.state = {
+			data: ''
+		};
+
+	};
+
+	componentWillMount() {
+		let response = this.request()
+		this.setState({
+			data: response,
+		});
+	}
+
+	request = (buName) => {
+		let apiUrl
+		let response
+		if (buName) {
+			apiUrl = 'https://api.github.com/getBUAssetAllocation';
+			//response = await axios.get(apiUrl, { BU_Name: buName });
+			if (buName === 'Computer Department') {
+				response = dummyData['buAssetAllocation1']
+			}
+			if (buName === 'Electronics Department') {
+				response = dummyData['buAssetAllocation2']
+			}
+			if (buName === 'Production Department') {
+				response = dummyData['buAssetAllocation3']
+			}
+		} else {
+			apiUrl = 'https://api.github.com/getAssetAllocation';
+			//response = await axios.get(apiUrl);
+			response = dummyData['assetAllocation']
+		}
+		let data = []
+		for (let i in response.responseData.allocationDetails) {
+			let item = {}
+			item.name = response.responseData.allocationDetails[i].name
+			item.value = response.responseData.allocationDetails[i].assetAllotted
+			data.push(item)
+		}
+		return data
+	}
+
+	onChangeBar(data) {
+
+		let response = this.request(data)
+
+		this.setState({
+			data: response,
+		});
+	};
+
 	render() {
 		return (
-			<PieChart width={400} height={400}>
-				<Pie
-					data={data}
+			<div>
+
+				<HSBar
+					showTextDown
+					outlineWidth= {0.5}
+					id="hsbarExample"
+					data={this.request()}
 					cx={200}
 					cy={200}
-					labelLine={false}
-					label={renderCustomizedLabel}
-					outerRadius={80}
-					fill="#8884d8"
-					dataKey="value"
-				>
-					{
-						data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
+					onClick={e => {
+						// this.setState(state => ({
+						// 	...state,
+						// 	value: e.bar.value
+						// }))
+						this.onChangeBar(e.bar.name);
 					}
-				</Pie>
-			</PieChart>
+					}
+				/>
+
+				<PieChart width={400} height={400}>
+					<Pie
+						data={this.state.data}
+						cx={200}
+						cy={200}
+						labelLine={false}
+						label={renderCustomizedLabel}
+						outerRadius={80}
+						fill="#8884d8"
+						dataKey="value"
+					>
+						{
+							this.state.data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
+						}
+					</Pie>
+				</PieChart>
+			</div>
+
 		);
 	}
 }
